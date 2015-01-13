0c750c593ca736103ecbf80fa928d88dd3b71d12
pdgraham <pdgraham@mit.edu>
2015-01-13 10:59:42 -0800

    Deleted old testing scripts

--- flycam2.py
+++ flycam2.py
@@ -1,6 +1,6 @@
-#import cv2
-#import numpy as np
-import urllib
+import cv2
+import numpy as np
+import urllib2
 import socket
 import sys
 import struct
@@ -17,7 +17,19 @@ class Walkera:
         self.elev = 0x044a
         self.aile =  0x044a
 
-        self.stream=urllib.urlopen('http://admin:admin123@192.168.10.1:8080/?action=stream')
+        password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+
+        top_level_url = "http://192.168.10.1:8080"
+        password_mgr.add_password(None, top_level_url, 'admin', 'admin123')
+
+        handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+        opener = urllib2.build_opener(handler)
+        opener.open("http://192.168.10.1:8080/?action=stream")
+        urllib2.install_opener(opener)
+        print 'opening url'
+        self.resp = urllib2.urlopen("http://192.168.10.1:8080/?action=stream")
+
+        # self.stream=urllib.urlopen('http://admin:admin123@192.168.10.1:8080/?action=stream')
     
         self.data = bytearray(18)
 
@@ -45,29 +57,71 @@ class Walkera:
         self.data[16] = self.elev & 0xff
         self.data[17] = sum(self.data[0:17]) & 0xFF
         
-        
+    def readframes(resp, recv_buffer=4096, delim='\n'):
+        buffer = ''
+        while data:
+            data = resp.read(recv_buffer)
+            buffer += data
+
+            while buffer.find(delim) != -1:
+                line, buffer = buffer.split("\n", 1)
+                if state==0:
+                    if line[0:20] == "--boundarydonotcross":
+                        state = 1
+                elif state==1:
+                    print line.split(":")
+                    state = 2
+                elif state==2:
+                    print line
+                    datalength = int(line.split(":")[1][1:-1])
+                    state = 3
+                    print "datalen", datalength
+                    #print buffer
+                elif state==3:
+                    state = 4
+                    
+                    timestamp = float(line.split(":")[1][1:-1])
+                    print "timestamp:", timestamp
+                    print "lag", timestamp - ts, 1/( timestamp - ts)
+                    ts = timestamp
+                else:
+                    while(len(buffer) < datalength):
+                        bytes_remaining = datalength - len(buffer)
+                        data = resp.read(bytes_remaining)
+                        buffer += data
+                    state = 0
+                # bytes+=self.stream.read(1080)
+                a = data.find('\xff\xd8')
+                b = data.find('\xff\xd9')
+                if a!=-1 and b!=-1:
+                    jpg = data[a:b+2]
+                    data= data[b+2:]
+                    self.frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),cv2.CV_LOAD_IMAGE_GRAYSCALE)
+                yield buffer
+        return
         
     def loop(self):
         self.threadOn = True
         while self.threadOn:
             self.update()
             self.sock.send(self.data) 
-            bytes+=self.stream.read(1080)
-            a = bytes.find('\xff\xd8')
-            b = bytes.find('\xff\xd9')
-            if a!=-1 and b!=-1:
-                jpg = bytes[a:b+2]
-                bytes= bytes[b+2:]
-                frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),cv2.CV_LOAD_IMAGE_GRAYSCALE)
-                if frame!= None:
-                    cv2.imshow('authenticated cam',frame)
-                key = cv2.waitKey(1)
-                if key ==27: # 'esc' key
-                    w.threadOn = 0
-                if key == 119: # 'w' key
-                    throttle = (throttle + 10) & 0xFF
-                if key == 115 and throttle > 1: # 's' key
-                    throttle -= 10
+            self.readframes(self.resp)
+            # bytes+=self.stream.read(1080)
+            # a = bytes.find('\xff\xd8')
+            # b = bytes.find('\xff\xd9')
+            # if a!=-1 and b!=-1:
+            #     jpg = bytes[a:b+2]
+            #     bytes= bytes[b+2:]
+            #     frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8),cv2.CV_LOAD_IMAGE_GRAYSCALE)
+            if self.frame!= None:
+                cv2.imshow('authenticated cam',self.frame)
+            key = cv2.waitKey(1)
+            if key ==27: # 'esc' key
+                w.threadOn = 0
+            if key == 119: # 'w' key
+                throttle = (throttle + 10) & 0xFF
+            if key == 115 and throttle > 1: # 's' key
+                throttle -= 10
             time.sleep(.03)
         self.sock.close()
    
