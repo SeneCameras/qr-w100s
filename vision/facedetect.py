17c96d8ff17a80486aecef62678db533d64a3a6a
Amir Hirsch <amir@flyingselfie.com>
2015-01-16 12:32:20 -0800

    lots o changes

--- vision/facedetect.py
+++ vision/facedetect.py
@@ -15,37 +15,39 @@ class FaceDetectProcess(Process):
         self.frontalclassifier = cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
 
         while self.running:
-            #print "blocking until we get a buffer"
-            i = self.bufferqueue.get(True) #block until new raw_frame
-            #print "buffer got"
-            startd = time.time()
-        
-            #i= cv2.imdecode(np.fromstring(raw_frame.value, dtype=np.uint8),1)
-            start = time.time()
-        
+            try:
+                #print "blocking until we get a buffer"
+                i = self.bufferqueue.get(True) #block until new raw_frame
+                #print "buffer got"
+                startd = time.time()
+            
+                #i= cv2.imdecode(np.fromstring(raw_frame.value, dtype=np.uint8),1)
+                start = time.time()
+            
 
-            minisize = (i.shape[1]/self.DOWNSCALE,i.shape[0]/self.DOWNSCALE)
-            miniframe = cv2.resize(i, minisize)
-            frontalfaces = self.frontalclassifier.detectMultiScale(miniframe)
-            for f in frontalfaces:
-                x, y, w, h = [ v*self.DOWNSCALE for v in f ]
-            #     # draws bounding box
-                cv2.rectangle(i, (x,y), (x+w,y+h), (0,0,255))
-            if len(frontalfaces) >= 1:
-                x, y, w, h = [ v*self.DOWNSCALE for v in frontalfaces[0] ]
-                if i.shape[1]*(2/3.) < x+w/2:# too far right
+                minisize = (i.shape[1]/self.DOWNSCALE,i.shape[0]/self.DOWNSCALE)
+                miniframe = cv2.resize(i, minisize)
+                frontalfaces = self.frontalclassifier.detectMultiScale(miniframe)
+                for f in frontalfaces:
+                    x, y, w, h = [ v*self.DOWNSCALE for v in f ]
+                #     # draws bounding box
                     cv2.rectangle(i, (x,y), (x+w,y+h), (0,0,255))
-            #         # print "turn counterclockwise"
-                elif i.shape[1]*(1/3.) > x+w/2: # too far left
-            #         # print "turn clockwise"
-                    cv2.rectangle(i, (x,y), (x+w,y+h), (0,255,0))
-                else: # centered
-            #         # print "centered"
-                    cv2.rectangle(i, (x,y), (x+w,y+h), (255,0,0))
-            #     print (x+w/2.),(y+h/2.),(w**2+h**2)**0.5
-            end = time.time()
-            print 'fd latency', (end-start)*1000, 'ms', (end-startd)*1000, 'ms'
-            
-            cv2.imshow('face_detect', i)
-            key = cv2.waitKey(1)
-            
+                if len(frontalfaces) >= 1:
+                    x, y, w, h = [ v*self.DOWNSCALE for v in frontalfaces[0] ]
+                    if i.shape[1]*(2/3.) < x+w/2:# too far right
+                        cv2.rectangle(i, (x,y), (x+w,y+h), (0,0,255))
+                #         # print "turn counterclockwise"
+                    elif i.shape[1]*(1/3.) > x+w/2: # too far left
+                #         # print "turn clockwise"
+                        cv2.rectangle(i, (x,y), (x+w,y+h), (0,255,0))
+                    else: # centered
+                #         # print "centered"
+                        cv2.rectangle(i, (x,y), (x+w,y+h), (255,0,0))
+                #     print (x+w/2.),(y+h/2.),(w**2+h**2)**0.5
+                end = time.time()
+                #print 'fd latency', (end-start)*1000, 'ms', (end-startd)*1000, 'ms'
+                
+                cv2.imshow('face_detect', i)
+                key = cv2.waitKey(1)
+            except Exception, e:
+                print e
--- vision/lk.py
+++ vision/lk.py
@@ -12,9 +12,10 @@ feature_params = dict( maxCorners = 50,
                                minDistance = 7,
                                blockSize = 7 )
 class LKProcess(Process):
-    def __init__(self, bufferqueue):
+    def __init__(self, bufferqueue, velq):
         Process.__init__(self)
         self.bufferqueue = bufferqueue
+        self.velocityqueue = velq
         self.running = True
         self.track_len = 10
         self.detect_interval = 5
@@ -26,66 +27,73 @@ class LKProcess(Process):
 
     def run(self):
         while self.running:
-            #print "blocking until we get a buffer"
-            frame = self.bufferqueue.get(True) #block until new raw_frame
-            #print "buffer got"
-            startd = time.time()
-            #frame = cv2.imdecode(np.fromstring(raw_frame.value, dtype=np.uint8),1)
-            
-            #TODO: sync the decoded image instead of raw jpeg
-            
-            start = time.time()
-            vis = frame.copy()
-            self.frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            if len(self.tracks) > 0:
-                img0, img1 = self.prev_gray, self.frame_gray
-                p0 = np.float32([tr[-1] for tr in self.tracks]).reshape(-1, 1, 2)
-                p1, st, err = cv2.calcOpticalFlowPyrLK(img0, img1, p0, None, **lk_params)
-                p0r, st, err = cv2.calcOpticalFlowPyrLK(img1, img0, p1, None, **lk_params)
-                d = abs(p0-p0r).reshape(-1, 2).max(-1)
-                good = d < 1
-                new_tracks = []
-                deltax = []
-                deltay = []
-                dx = 0
-                dy = 0
+            try:
+                #print "blocking until we get a buffer"
+                frame = self.bufferqueue.get(True) #block until new raw_frame
+                #print "buffer got"
+                startd = time.time()
+                #frame = cv2.imdecode(np.fromstring(raw_frame.value, dtype=np.uint8),1)
                 
-                for tr, (x, y), good_flag in zip(self.tracks, p1.reshape(-1, 2), good):
-                    if not good_flag:
-                        continue
-                    deltax.append(x - tr[-1][0]) 
-                    deltay.append(y - tr[-1][1])
-                    tr.append((x, y))
-
+                #TODO: sync the decoded image instead of raw jpeg
+                
+                start = time.time()
+                vis = frame.copy()
+                self.frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                if len(self.tracks) > 0:
+                    img0, img1 = self.prev_gray, self.frame_gray
+                    p0 = np.float32([tr[-1] for tr in self.tracks]).reshape(-1, 1, 2)
+                    p1, st, err = cv2.calcOpticalFlowPyrLK(img0, img1, p0, None, **lk_params)
+                    p0r, st, err = cv2.calcOpticalFlowPyrLK(img1, img0, p1, None, **lk_params)
+                    d = abs(p0-p0r).reshape(-1, 2).max(-1)
+                    good = d < 1
+                    new_tracks = []
+                    deltax = []
+                    deltay = []
+                    roll = []
+                    dx = 0
+                    dy = 0
                     
-                    #print tr
-                    if len(tr) > self.track_len:
-                        del tr[0]
-                    new_tracks.append(tr)
-                    cv2.circle(vis, (x, y), 2, (0, 255, 0), -1)
-                self.tracks = new_tracks
-                cv2.polylines(vis, [np.int32(tr) for tr in self.tracks], False, (0, 255, 0))
-                draw_str(vis, (20, 20), 'track count: %d' % len(self.tracks))
-                if len(deltay)>0:
-                    pass
-                    #print "avg:",(sum(deltax)/len(deltax),sum(deltay)/len(deltay))
-        #            print deltax
-        #           print deltay
-            if self.frame_idx % self.detect_interval == 0:
-                mask = np.zeros_like(self.frame_gray)
-                mask[:] = 255
-                for x, y in [np.int32(tr[-1]) for tr in self.tracks]:
-                    cv2.circle(mask, (x, y), 5, 0, -1)
-                p = cv2.goodFeaturesToTrack(self.frame_gray, mask = mask, **feature_params)
-                if p is not None:
-                    for x, y in np.float32(p).reshape(-1, 2):
-                        self.tracks.append([(x, y)])
-            self.frame_idx += 1
-            self.prev_gray = self.frame_gray
-            end = time.time()
-            print 'lk latency', (end-start)*1000, 'ms', (end-startd)*1000, 'ms'
-            #vis
-            cv2.imshow('lk_track', vis)
-        
-        
-            cv2.waitKey(1)
+                    for tr, (x, y), good_flag in zip(self.tracks, p1.reshape(-1, 2), good):
+                        if not good_flag:
+                            continue
+                        dx = x - tr[-1][0]
+                        dy = y - tr[-1][1]
+                        deltax.append(dx) 
+                        deltay.append(dy)
+                        tr.append((x, y))
+                        roll.append(x * dy - y * dx)
+                        
+                        #print tr
+                        if len(tr) > self.track_len:
+                            del tr[0]
+                        new_tracks.append(tr)
+                        cv2.circle(vis, (x, y), 2, (0, 255, 0), -1)
+                    self.tracks = new_tracks
+                    cv2.polylines(vis, [np.int32(tr) for tr in self.tracks], False, (0, 255, 0))
+                    draw_str(vis, (20, 20), 'track count: %d' % len(self.tracks))
+                    if len(deltay)>0:
+                        #pass
+                        self.velocityqueue.put((deltax, deltay, roll))
+                        #print "avg:",(sum(deltax)/len(deltax),sum(deltay)/len(deltay)), "roll", (sum(roll)/len(roll))
+            #            print deltax
+            #           print deltay
+                if self.frame_idx % self.detect_interval == 0:
+                    mask = np.zeros_like(self.frame_gray)
+                    mask[:] = 255
+                    for x, y in [np.int32(tr[-1]) for tr in self.tracks]:
+                        cv2.circle(mask, (x, y), 5, 0, -1)
+                    p = cv2.goodFeaturesToTrack(self.frame_gray, mask = mask, **feature_params)
+                    if p is not None:
+                        for x, y in np.float32(p).reshape(-1, 2):
+                            self.tracks.append([(x, y)])
+                self.frame_idx += 1
+                self.prev_gray = self.frame_gray
+                end = time.time()
+                #print 'lk latency', (end-start)*1000, 'ms', (end-startd)*1000, 'ms'
+                #vis
+                cv2.imshow('lk_track', vis)
+            
+            
+                cv2.waitKey(1)
+            except Exception, e:
+                print "exception in lk:", e
