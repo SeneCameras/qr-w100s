9c9ddf028f44a56bffb4a94314b6b14254afcc7b
rjwalters <robb@flyingselfie.com>
2015-03-03 17:25:52 -0800

    add threaded test to latencytest

--- qr-w100s/latencytest.py
+++ qr-w100s/latencytest.py
@@ -59,7 +59,7 @@ def pulse():
    print "zero: ", w.getString()
    sys.stdout.flush()
    
-   for t in range(100):
+   for t in range(10):
       data = [0, t/100.0, 0, 0]
       w.thrust = (int(     data[1] * (0x05dc-0x02bf)    ) + 0x02bf)
       w.pitch  = (int((1 - data[3])*((0x0640-0x025b)>>1)) + 0x025b)
@@ -72,7 +72,7 @@ def pulse():
          sys.stdout.flush()
       time.sleep(0.01)
       
-   for t in range(100):
+   for t in range(10):
       data = [0, 1.0 - t/100.0, 0, 0]
       w.thrust = (int(     data[1] * (0x05dc-0x02bf)    ) + 0x02bf)
       w.pitch  = (int((1 - data[3])*((0x0640-0x025b)>>1)) + 0x025b)
@@ -95,7 +95,53 @@ def pulse():
    print "*****************  end pulse  *****************"
    sys.stdout.flush()
    
+from threading import Thread
 
+class Levels(Thread):
+   def __init__(self):
+      Thread.__init__(self)
+  
+      self.w = WalkeraCommand()
+      self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      self.s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1) #disable Nagle          
+      #self.s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 0)   #disable kernel buffer
+      self.s.connect(("192.168.10.1", 2001))
+      #self.s.setblocking(0)
+   
+      self.w.zero()
+      self.s.send(self.w.data)
+      
+      print "*****************  thread initialized  *****************"
+      sys.stdout.flush()
+      
+      time.sleep(0.5)
+      
+   def run(self):
+      while True:
+         data = [0, 1.0, 0, 0]
+         self.w.thrust = (int(     data[1] * (0x05dc-0x02bf)    ) + 0x02bf)
+         self.w.pitch  = (int((1 - data[3])*((0x0640-0x025b)>>1)) + 0x025b)
+         self.w.roll   = (int((1 - data[2])*((0x0640-0x025b)>>1)) + 0x025b)
+         self.w.yaw    = (int((1 - data[0])*((0x0640-0x025b)>>1)) + 0x025b)
+         self.w.update()
+         self.s.send(self.w.data)
+         
+         sys.stdout.write("+")
+         sys.stdout.flush()
+         #time.sleep(1.0)
+         
+         data = [0, 0.0, 0, 0]
+         self.w.thrust = (int(     data[1] * (0x05dc-0x02bf)    ) + 0x02bf)
+         self.w.pitch  = (int((1 - data[3])*((0x0640-0x025b)>>1)) + 0x025b)
+         self.w.roll   = (int((1 - data[2])*((0x0640-0x025b)>>1)) + 0x025b)
+         self.w.yaw    = (int((1 - data[0])*((0x0640-0x025b)>>1)) + 0x025b)
+         self.w.update()
+         self.s.send(self.w.data)
+         
+         sys.stdout.write("-")
+         sys.stdout.flush()
+         #time.sleep(1.0)
+         
 if __name__ == '__main__':
    
    app = QtGui.QApplication(sys.argv)
@@ -103,10 +149,15 @@ if __name__ == '__main__':
    app.aboutToQuit.connect(widget.shutdown)
    widget.show()
 
-   FPS = 0.1
+   '''
+   FPS = 4
    t = QtCore.QTimer()
    t.timeout.connect(pulse)
    t.start(1000.0/FPS)
+   '''
+   
+   output_thread = Levels()
+   output_thread.start()
    
    sys.exit(app.exec_())
    
